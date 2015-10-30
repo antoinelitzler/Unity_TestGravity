c14b70b753eab474f335f8bdb89a24c01f31fde2
Antoine Litzler <antoinelitzler@hotmail.com>
2015-10-30 17:24:17 +0100

    First Commit

---
+++ Assets/Scripts/GravityChanger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityChanger : MonoBehaviour {
+
+    Rigidbody rb;
+    CharacterController cc;
+    Vector3 currentGravity;
+
+    bool isOnGravRoad = false;
+    Vector3 lastGroundPosition;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+        currentGravity = Physics.gravity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.WakeUp();
+        if( !cc.isGrounded && isOnGravRoad)
+        {
+            transform.position = lastGroundPosition;
+        }
+        else
+        {
+            lastGroundPosition = transform.position;
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if ( hit.controller.isGrounded ) {
+
+            if (hit.collider.tag == "GravityRoad")
+            {
+                isOnGravRoad = true;
+                Debug.Log(currentGravity + " " + currentGravity.magnitude);
+
+                if (hit.normal.normalized != -currentGravity.normalized)
+                {
+                    currentGravity = -(hit.normal.normalized) * 9.81f;
+                    Physics.gravity = currentGravity;
+                }
+
+            }
+            else
+            {
+                isOnGravRoad = false;
+            }
+        }
+    }
+
+}
